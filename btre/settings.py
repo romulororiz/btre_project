0436fcb5ac9e41bc9967f17e4f994d4184423f2e
Romulo Nunes de Menezes Roriz <rnmroriz@gmail.com>
2020-10-19 23:24:27 +0200

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -143,3 +143,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rnmroriz@gmail.com'
 EMAIL_HOST_PASSWORD = 'romuloelorena24'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass 
